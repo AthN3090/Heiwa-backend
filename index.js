61ad049e06dfd5fffc790d9005cd7a3791ded016
AthN3090 <amandevrishi12@gmail.com>
2022-09-12 19:41:43 +0530

    Port change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const http = require('http')
 const cors = require('cors')
-const PORT = 3001
+const PORT = 8000 || process.env.PORT
 app.use(cors())
 const {Server} = require('socket.io')
 const { resolve } = require('path')
